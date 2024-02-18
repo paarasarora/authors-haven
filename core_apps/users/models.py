0a806692b4d94ec412c2d62d2adca6fb65a4bf8e
paaras arora <88595024+paarasarora@users.noreply.github.com>
2024-02-19 04:40:07 +0530

    feat:add custom user model

--- core_apps/users/models.py
+++ core_apps/users/models.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
-from .managers import CustomUserManager
+from .managers import CustomUsermanager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -26,7 +25,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
 
-    objects = CustomUserManager()
+    objects = CustomUsermanager()
 
     class Meta:
         verbose_name = _("user")
