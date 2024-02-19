4b1e5a518bb77adf64764e3273c8a473677e1256
paaras arora <88595024+paarasarora@users.noreply.github.com>
2024-02-20 01:00:36 +0530

    dev: rooting staticfiles through nginx

--- authorsHaven/settings/base.py
+++ authorsHaven/settings/base.py
@@ -146,8 +146,13 @@ ADMIN_URL = 'supersecret/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = str(ROOT_DIR / 'staticfiles')
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = str(ROOT_DIR / "staticfiles")
+STATICFILES_DIRS = []
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 MEDIA_URL = '/mediafiles/'
 MEIA_ROOT = str(ROOT_DIR / 'mediafiles')
