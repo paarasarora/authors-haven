cc299e449df17b9646357a110d0fe757961e42d8
paaras arora <88595024+paarasarora@users.noreply.github.com>
2024-03-26 16:56:06 +0530

    dev: profile api's

--- authorsHaven/urls.py
+++ authorsHaven/urls.py
@@ -12,7 +12,7 @@ schema_view = get_schema_view(
         title="Authors API",
         default_version="v1",
         description="API endpoints for the Authors API course",
-        contact=openapi.Contact(email="api.imperfect@gmail.com"),
+        contact=openapi.Contact(email="paarasarora2@gmail.com"),
         license=openapi.License(name="MIT License"),
     ),
     public=True,
@@ -26,5 +26,6 @@ urlpatterns = [
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path(settings.ADMIN_URL, admin.site.urls),
     re_path('api/v1/', include(router.urls)),
-    re_path('api/v1/', include('core_apps.users.urls', namespace='users'))
+    re_path('api/v1/', include('core_apps.users.urls', namespace='users')),
+    path("api/v1/profiles/", include("core_apps.profiles.urls")),
 ]
